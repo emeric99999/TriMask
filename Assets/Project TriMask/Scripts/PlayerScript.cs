13f5ae21dce42eb456eaee7e5f63e7bd40b77113
emeric99999 <emeric.paturel@gmail.com>
2024-11-04 17:31:27 +0100

    first tests (camera & mask change)

--- Assets/Project TriMask/Scripts/PlayerScript.cs
+++ Assets/Project TriMask/Scripts/PlayerScript.cs
@@ -13,41 +13,65 @@ public class Player : MonoBehaviour
 
     [SerializeField] private InputActionReference _moveactions;
 
+    public KeyCode _switchKey; //touche utilisée pour changer de mode 
+    public int _modeID = 0; // id du mode actuel (0 : double jamp, 1 : invisibilité, 2 : rayon X
+    public SpriteRenderer uiSprite;
+
     private void Update()
     {
         Move();
-    }
+        if (_modeID == 0) { } //doublejump 
+        else if (_modeID == 1) { } //invisibility 
+        else { } //detection
 
-    private void Move()
-    {
-        transform.position = (Vector2)transform.position + _direction * _playerSpeed * Time.deltaTime; // * direction 
-    }
+        setModeID();
+        
+       
 
-    private void OnMoveAction(InputAction.CallbackContext ctx)
+            }
+
+    private void setModeID()   // fonction qui change le _modeID lorsque la touche switch est pressée
     {
-        if (ctx.performed)
-        {
-            _direction = ctx.ReadValue<Vector2>();
+        if (Input.GetKeyDown(_switchKey))
+        { _modeID += 1;
+            _modeID = _modeID % 3;
+            if (_modeID == 0) { uiSprite.color = Color.white; }
+            else if (_modeID == 1) {  uiSprite.color = Color.red; }
+            else { uiSprite.color = Color.green; }
         }
+    }
 
-        if (ctx.canceled)
-        {
-            _direction = Vector2.zero;
+
+            private void Move()
+            {
+                transform.position = (Vector2)transform.position + _direction * _playerSpeed * Time.deltaTime; // * direction 
+            }
+
+            private void OnMoveAction(InputAction.CallbackContext ctx)
+            {
+                if (ctx.performed)
+                {
+                    _direction = ctx.ReadValue<Vector2>();
+                }
+
+                if (ctx.canceled)
+                {
+                    _direction = Vector2.zero;
+                }
+            }
+
+            private void OnEnable()
+            {
+                _moveactions.action.performed += OnMoveAction;
+                _moveactions.action.canceled += OnMoveAction;
+            }
+
+            private void OnDisable()
+            {
+                _moveactions.action.performed -= OnMoveAction;
+                _moveactions.action.canceled -= OnMoveAction;
+            }
+
+
+
         }
-    }
-
-    private void OnEnable()
-    {
-        _moveactions.action.performed += OnMoveAction;
-        _moveactions.action.canceled += OnMoveAction;
-    }
-
-    private void OnDisable()
-    {
-        _moveactions.action.performed -= OnMoveAction;
-        _moveactions.action.canceled -= OnMoveAction;
-    }
-
-
-
-}
