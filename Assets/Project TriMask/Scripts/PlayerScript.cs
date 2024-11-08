6fd73cb0335a1353ec16113cf28edc1385af31ba
Benjamin <82211851+gitbenji-prog@users.noreply.github.com>
2024-11-08 11:19:26 +0100

    7/11 Ajout du jump ( pas fini )+ Collisions et gravité

--- Assets/Project TriMask/Scripts/PlayerScript.cs
+++ Assets/Project TriMask/Scripts/PlayerScript.cs
@@ -17,11 +17,14 @@ public class Player : MonoBehaviour
     public int _modeID = 0; // id du mode actuel (0 : double jump, 1 : invisibilité, 2 : rayon X
     [SerializeField] private SpriteRenderer uiSprite;
     [SerializeField] private SpriteRenderer playerSprite;
-    private List<Sprite> currentSpriteList ;
+    private List<Sprite> currentSpriteList;
     [SerializeField] private List<Sprite> doubleJumpSpriteList;
     [SerializeField] private List<Sprite> xraySpriteList;
     [SerializeField] private List<Sprite> stealthSpriteList;
+    [SerializeField] private Rigidbody2D _playerRigidbody;
+    [SerializeField] private BoxCollider2D _playerCollider;
     private float timer = 0;
+    [SerializeField] private float animTime; 
 
 
     public void Start()
@@ -31,15 +34,16 @@ public class Player : MonoBehaviour
     private void Update()
     {
         Move();
+        Jump();
         if (_modeID == 0) { } //doublejump 
         else if (_modeID == 1) { } //invisibility 
         else { } //detection
 
         setModeID();
-        
-       
 
-            }
+
+
+    }
 
     private void setModeID()   // fonction qui change le _modeID lorsque la touche switch est pressée
     {
@@ -47,24 +51,24 @@ public class Player : MonoBehaviour
         { _modeID += 1;
             _modeID = _modeID % 3;
             if (_modeID == 0) { uiSprite.color = Color.white; currentSpriteList = doubleJumpSpriteList; }
-            else if (_modeID == 1) {  uiSprite.color = Color.red; currentSpriteList = xraySpriteList; }
+            else if (_modeID == 1) { uiSprite.color = Color.red; currentSpriteList = xraySpriteList; }
             else { uiSprite.color = Color.green; currentSpriteList = stealthSpriteList; }
         }
     }
 
 
-            private void Move()
-            {
-                transform.position = (Vector2)transform.position + _direction * _playerSpeed * Time.deltaTime; // * direction 
+    private void Move()
+    {
+        transform.position = (Vector2)transform.position + _direction * _playerSpeed * Time.deltaTime; // * direction 
 
         if (_direction != Vector2.zero)
         {
-            if (timer < 1f)
+            if (timer < animTime)
             {
                 timer += Time.deltaTime;
-                if (timer < 0.25f) playerSprite.sprite = currentSpriteList[1];
-                else if (timer < 0.5f) playerSprite.sprite = currentSpriteList[2];
-                else if (timer < 0.75f) playerSprite.sprite = currentSpriteList[3];
+                if (timer < animTime/4) playerSprite.sprite = currentSpriteList[1];
+                else if (timer < animTime/2) playerSprite.sprite = currentSpriteList[2];
+                else if (timer < 3*animTime/4) playerSprite.sprite = currentSpriteList[3];
                 else playerSprite.sprite = currentSpriteList[4];
             }
 
@@ -75,31 +79,42 @@ public class Player : MonoBehaviour
 
     }
 
-            private void OnMoveAction(InputAction.CallbackContext ctx)
-            {
-                if (ctx.performed)
-                {
-                    _direction = ctx.ReadValue<Vector2>();
-                }
+    private void OnMoveAction(InputAction.CallbackContext ctx)
+    {
+        if (ctx.performed)
+        {
+            _direction = ctx.ReadValue<Vector2>();
+        }
 
-                if (ctx.canceled)
-                {
-                    _direction = Vector2.zero;
+        if (ctx.canceled)
+        {
+            _direction = Vector2.zero;
 
-                }
-            }
+        }
+    }
 
-            private void OnEnable()
-            {
-                _moveactions.action.performed += OnMoveAction;
-                _moveactions.action.canceled += OnMoveAction;
-            }
+    private void OnEnable()
+    {
+        _moveactions.action.performed += OnMoveAction;
+        _moveactions.action.canceled += OnMoveAction;
+    }
 
-            private void OnDisable()
-            {
-                _moveactions.action.performed -= OnMoveAction;
-                _moveactions.action.canceled -= OnMoveAction;
-            }
+    private void OnDisable()
+    {
+        _moveactions.action.performed -= OnMoveAction;
+        _moveactions.action.canceled -= OnMoveAction;
+    }
+
+        private void Jump ()
+        {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            _playerRigidbody.AddForce(7*Vector2.up,ForceMode2D.Impulse);
+
+     
+
+        }
+        }
 
 
 
